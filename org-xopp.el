6423de8132ba7f3eef7fc3d261aba9c741bb7357
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-12-09 18:29:55 +0200

    fix commentary

--- org-xopp.el
+++ org-xopp.el
@@ -23,7 +23,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; This package is for making arbitrary links across text files.
+;; This package is for embedding xournalpp figures/pages in org-mode.
 
 ;;; Code:
 (require 'org-compat)
